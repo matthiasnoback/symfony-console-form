5387adc282ee5e1db6b05fe4e831ba72bb51fa3a
Remon van de Kamp <rpkamp@gmail.com>
2018-03-16 14:44:46 +0100

    Add support for Symfony's NumberType

---
+++ test/Form/PriceType.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Matthias\SymfonyConsoleForm\Tests\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class PriceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('price', NumberType::class);
+    }
+}
