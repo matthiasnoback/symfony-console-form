a7e1e84063c00330bcea65ae8c43fea309192238
Cristoforo Cervino <cristoforo.cervino@facile.it>
2022-02-03 16:15:36 +0100

    fix fatal error when using an object as default value in ChoiceType

--- src/Bridge/Transformer/ChoiceTransformer.php
+++ src/Bridge/Transformer/ChoiceTransformer.php
@@ -4,6 +4,7 @@ namespace Matthias\SymfonyConsoleForm\Bridge\Transformer;
 
 use Matthias\SymfonyConsoleForm\Console\Helper\Question\AlwaysReturnKeyOfChoiceQuestion;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Form\ChoiceList\View\ChoiceView;
 use Symfony\Component\Form\FormInterface;
 
 final class ChoiceTransformer extends AbstractTransformer
@@ -19,5 +20,22 @@ final class ChoiceTransformer extends AbstractTransformer
         }
 
         return $question;
+    }
+
+    protected function defaultValueFrom(FormInterface $form)
+    {
+        $defaultValue = parent::defaultValueFrom($form);
+
+        // $defaultValue could be an object
+        // Let's find out what form computed to be its view representation
+        $formView = $form->createView();
+        $defaultValueFromChoiceView = \array_reduce(
+            $formView->vars['choices'],
+            static fn(
+                ?string $carry,
+                ChoiceView $choiceView
+            ): ?string => (null === $carry && $choiceView->data === $defaultValue) ? $choiceView->value : $carry
+        );
+        return $defaultValueFromChoiceView ?? $defaultValue;
     }
 }
